9df61071ed111756febd3d148c430d60481d245b
gluttonyOwO <41547584+gluttonyOwO@users.noreply.github.com>
2024-09-04 06:59:47 +0800

    fix install_common with ui

--- ui_install_common.h
+++ ui_install_common.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'install_common.ui'
 **
-** Created by: Qt User Interface Compiler version 5.12.8
+** Created by: Qt User Interface Compiler version 5.15.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -11,7 +11,6 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QCheckBox>
 #include <QtWidgets/QComboBox>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QPushButton>
@@ -23,19 +22,8 @@ QT_BEGIN_NAMESPACE
 class Ui_install_common
 {
 public:
-    QLabel *label;
     QLabel *label_2;
     QComboBox *comboBox;
-    QCheckBox *checkBox_4;
-    QCheckBox *checkBox;
-    QCheckBox *checkBox_2;
-    QCheckBox *checkBox_5;
-    QCheckBox *checkBox_3;
-    QCheckBox *checkBox_10;
-    QCheckBox *checkBox_7;
-    QCheckBox *checkBox_8;
-    QCheckBox *checkBox_6;
-    QCheckBox *checkBox_9;
     QLabel *label_3;
     QComboBox *comboBox_2;
     QPushButton *pushButton;
@@ -51,45 +39,12 @@ public:
         if (install_common->objectName().isEmpty())
             install_common->setObjectName(QString::fromUtf8("install_common"));
         install_common->resize(980, 700);
-        label = new QLabel(install_common);
-        label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(20, 150, 121, 17));
         label_2 = new QLabel(install_common);
         label_2->setObjectName(QString::fromUtf8("label_2"));
         label_2->setGeometry(QRect(30, 10, 67, 17));
         comboBox = new QComboBox(install_common);
         comboBox->setObjectName(QString::fromUtf8("comboBox"));
         comboBox->setGeometry(QRect(150, 10, 501, 25));
-        checkBox_4 = new QCheckBox(install_common);
-        checkBox_4->setObjectName(QString::fromUtf8("checkBox_4"));
-        checkBox_4->setGeometry(QRect(440, 150, 121, 23));
-        checkBox = new QCheckBox(install_common);
-        checkBox->setObjectName(QString::fromUtf8("checkBox"));
-        checkBox->setGeometry(QRect(140, 150, 92, 23));
-        checkBox_2 = new QCheckBox(install_common);
-        checkBox_2->setObjectName(QString::fromUtf8("checkBox_2"));
-        checkBox_2->setGeometry(QRect(240, 150, 92, 23));
-        checkBox_5 = new QCheckBox(install_common);
-        checkBox_5->setObjectName(QString::fromUtf8("checkBox_5"));
-        checkBox_5->setGeometry(QRect(570, 150, 92, 23));
-        checkBox_3 = new QCheckBox(install_common);
-        checkBox_3->setObjectName(QString::fromUtf8("checkBox_3"));
-        checkBox_3->setGeometry(QRect(340, 150, 92, 23));
-        checkBox_10 = new QCheckBox(install_common);
-        checkBox_10->setObjectName(QString::fromUtf8("checkBox_10"));
-        checkBox_10->setGeometry(QRect(570, 180, 92, 23));
-        checkBox_7 = new QCheckBox(install_common);
-        checkBox_7->setObjectName(QString::fromUtf8("checkBox_7"));
-        checkBox_7->setGeometry(QRect(240, 180, 121, 23));
-        checkBox_8 = new QCheckBox(install_common);
-        checkBox_8->setObjectName(QString::fromUtf8("checkBox_8"));
-        checkBox_8->setGeometry(QRect(380, 180, 61, 23));
-        checkBox_6 = new QCheckBox(install_common);
-        checkBox_6->setObjectName(QString::fromUtf8("checkBox_6"));
-        checkBox_6->setGeometry(QRect(140, 180, 92, 23));
-        checkBox_9 = new QCheckBox(install_common);
-        checkBox_9->setObjectName(QString::fromUtf8("checkBox_9"));
-        checkBox_9->setGeometry(QRect(440, 180, 111, 23));
         label_3 = new QLabel(install_common);
         label_3->setObjectName(QString::fromUtf8("label_3"));
         label_3->setGeometry(QRect(10, 50, 131, 17));
@@ -116,37 +71,14 @@ public:
         comboBox_2->addItem(QString());
         comboBox_2->addItem(QString());
         comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
-        comboBox_2->addItem(QString());
         comboBox_2->setObjectName(QString::fromUtf8("comboBox_2"));
         comboBox_2->setGeometry(QRect(150, 50, 501, 25));
         pushButton = new QPushButton(install_common);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(670, 10, 89, 25));
+        pushButton->setGeometry(QRect(660, 90, 89, 25));
         textEdit = new QTextEdit(install_common);
         textEdit->setObjectName(QString::fromUtf8("textEdit"));
-        textEdit->setGeometry(QRect(50, 240, 631, 401));
+        textEdit->setGeometry(QRect(50, 140, 631, 501));
         pushButton_2 = new QPushButton(install_common);
         pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
         pushButton_2->setGeometry(QRect(300, 650, 89, 25));
@@ -172,74 +104,40 @@ public:
 
     void retranslateUi(QWidget *install_common)
     {
-        install_common->setWindowTitle(QApplication::translate("install_common", "Form", nullptr));
-        label->setText(QApplication::translate("install_common", "Message Type:", nullptr));
-        label_2->setText(QApplication::translate("install_common", "Device:", nullptr));
-        checkBox_4->setText(QApplication::translate("install_common", "GroundDetect", nullptr));
-        checkBox->setText(QApplication::translate("install_common", "Distance", nullptr));
-        checkBox_2->setText(QApplication::translate("install_common", "Environment", nullptr));
-        checkBox_5->setText(QApplication::translate("install_common", "Image", nullptr));
-        checkBox_3->setText(QApplication::translate("install_common", "GPS", nullptr));
-        checkBox_10->setText(QApplication::translate("install_common", "MotorAxle", nullptr));
-        checkBox_7->setText(QApplication::translate("install_common", "MotorSteering", nullptr));
-        checkBox_8->setText(QApplication::translate("install_common", "UPS", nullptr));
-        checkBox_6->setText(QApplication::translate("install_common", "IMU", nullptr));
-        checkBox_9->setText(QApplication::translate("install_common", "WheelState", nullptr));
-        label_3->setText(QApplication::translate("install_common", "Node Config List:", nullptr));
-        comboBox_2->setItemText(0, QApplication::translate("install_common", "/V0/ultrasound_2_node", nullptr));
-        comboBox_2->setItemText(1, QApplication::translate("install_common", "/V0/ultrasound_3_node", nullptr));
-        comboBox_2->setItemText(2, QApplication::translate("install_common", "/V0/ultrasound_4_node", nullptr));
-        comboBox_2->setItemText(3, QApplication::translate("install_common", "/V0/ultrasound_5_node", nullptr));
-        comboBox_2->setItemText(4, QApplication::translate("install_common", "/V0/ultrasound_6_node", nullptr));
-        comboBox_2->setItemText(5, QApplication::translate("install_common", "/V0/ultrasound_7_node", nullptr));
-        comboBox_2->setItemText(6, QApplication::translate("install_common", "/V0/zed_0_node", nullptr));
-        comboBox_2->setItemText(7, QApplication::translate("install_common", "/V0/zed_1_node", nullptr));
-        comboBox_2->setItemText(8, QApplication::translate("install_common", "/V0/zed_3_node", nullptr));
-        comboBox_2->setItemText(9, QApplication::translate("install_common", "/V0/zed_4_node", nullptr));
-        comboBox_2->setItemText(10, QApplication::translate("install_common", "/V0/zed_5_node", nullptr));
-        comboBox_2->setItemText(11, QApplication::translate("install_common", "/V0/webcam_0_node", nullptr));
-        comboBox_2->setItemText(12, QApplication::translate("install_common", "/V0/webcam_1_node", nullptr));
-        comboBox_2->setItemText(13, QApplication::translate("install_common", "/V0/webcam_2_node", nullptr));
-        comboBox_2->setItemText(14, QApplication::translate("install_common", "/V0/webcam_3_node", nullptr));
-        comboBox_2->setItemText(15, QApplication::translate("install_common", "/V0/webcam_4_node", nullptr));
-        comboBox_2->setItemText(16, QApplication::translate("install_common", "/V0/webcam_5_node", nullptr));
-        comboBox_2->setItemText(17, QApplication::translate("install_common", "/V0/chassis_steering_1_node", nullptr));
-        comboBox_2->setItemText(18, QApplication::translate("install_common", "/V0/chassis_steering_2_node", nullptr));
-        comboBox_2->setItemText(19, QApplication::translate("install_common", "/V0/chassis_steering_3_node", nullptr));
-        comboBox_2->setItemText(20, QApplication::translate("install_common", "/V0/chassis_steering_4_node", nullptr));
-        comboBox_2->setItemText(21, QApplication::translate("install_common", "/V0/chassis_steering_5_node", nullptr));
-        comboBox_2->setItemText(22, QApplication::translate("install_common", "/V0/chassis_steering_6_node", nullptr));
-        comboBox_2->setItemText(23, QApplication::translate("install_common", "/V0/chassis_steering_7_node", nullptr));
-        comboBox_2->setItemText(24, QApplication::translate("install_common", "/V0/chassis_battery_0_node", nullptr));
-        comboBox_2->setItemText(25, QApplication::translate("install_common", "/V0/chassis_battery_1_node", nullptr));
-        comboBox_2->setItemText(26, QApplication::translate("install_common", "/V0/chassis_battery_2_node", nullptr));
-        comboBox_2->setItemText(27, QApplication::translate("install_common", "/V0/chassis_battery_3_node", nullptr));
-        comboBox_2->setItemText(28, QApplication::translate("install_common", "/V0/chassis_battery_4_node", nullptr));
-        comboBox_2->setItemText(29, QApplication::translate("install_common", "/V0/chassis_battery_5_node", nullptr));
-        comboBox_2->setItemText(30, QApplication::translate("install_common", "/V0/chassis_battery_6_node", nullptr));
-        comboBox_2->setItemText(31, QApplication::translate("install_common", "/V0/chassis_battery_7_node", nullptr));
-        comboBox_2->setItemText(32, QApplication::translate("install_common", "/V0/chassis_battery_8_node", nullptr));
-        comboBox_2->setItemText(33, QApplication::translate("install_common", "/V0/chassis_battery_9_node", nullptr));
-        comboBox_2->setItemText(34, QApplication::translate("install_common", "/V0/controlserver_0_node", nullptr));
-        comboBox_2->setItemText(35, QApplication::translate("install_common", "/V0/rfcomm_0_node", nullptr));
-        comboBox_2->setItemText(36, QApplication::translate("install_common", "/V0/rfcomm_1_node", nullptr));
-        comboBox_2->setItemText(37, QApplication::translate("install_common", "/V0/grounddetect_0_node", nullptr));
-        comboBox_2->setItemText(38, QApplication::translate("install_common", "/V0/grounddetect_1_node", nullptr));
-        comboBox_2->setItemText(39, QApplication::translate("install_common", "/V0/grounddetect_2_node", nullptr));
-        comboBox_2->setItemText(40, QApplication::translate("install_common", "/V0/grounddetect_3_node", nullptr));
-        comboBox_2->setItemText(41, QApplication::translate("install_common", "/V0/qosserver_0_node", nullptr));
-        comboBox_2->setItemText(42, QApplication::translate("install_common", "/V0/safetyserver_0_node", nullptr));
-        comboBox_2->setItemText(43, QApplication::translate("install_common", "/V0/timesyncserver_0_node", nullptr));
-        comboBox_2->setItemText(44, QApplication::translate("install_common", "/V0/dataserver_0_node", nullptr));
+        install_common->setWindowTitle(QCoreApplication::translate("install_common", "Form", nullptr));
+        label_2->setText(QCoreApplication::translate("install_common", "Device:", nullptr));
+        label_3->setText(QCoreApplication::translate("install_common", "Node Config List:", nullptr));
+        comboBox_2->setItemText(0, QCoreApplication::translate("install_common", "/V0/gps_0", nullptr));
+        comboBox_2->setItemText(1, QCoreApplication::translate("install_common", "/V0/ultrasound_1", nullptr));
+        comboBox_2->setItemText(2, QCoreApplication::translate("install_common", "/V0/ultrasound_2", nullptr));
+        comboBox_2->setItemText(3, QCoreApplication::translate("install_common", "/V0/zed_0", nullptr));
+        comboBox_2->setItemText(4, QCoreApplication::translate("install_common", "/V0/zed_1", nullptr));
+        comboBox_2->setItemText(5, QCoreApplication::translate("install_common", "/V0/zed_2", nullptr));
+        comboBox_2->setItemText(6, QCoreApplication::translate("install_common", "/V0/webcam_0", nullptr));
+        comboBox_2->setItemText(7, QCoreApplication::translate("install_common", "/V0/webcam_1", nullptr));
+        comboBox_2->setItemText(8, QCoreApplication::translate("install_common", "/V0/chassis_steering_0", nullptr));
+        comboBox_2->setItemText(9, QCoreApplication::translate("install_common", "/V0/chassis_steering_1", nullptr));
+        comboBox_2->setItemText(10, QCoreApplication::translate("install_common", "/V0/chassis_steering_2", nullptr));
+        comboBox_2->setItemText(11, QCoreApplication::translate("install_common", "/V0/chassis_steering_3", nullptr));
+        comboBox_2->setItemText(12, QCoreApplication::translate("install_common", "/V0/chassis_motor_0", nullptr));
+        comboBox_2->setItemText(13, QCoreApplication::translate("install_common", "/V0/chassis_motor_1", nullptr));
+        comboBox_2->setItemText(14, QCoreApplication::translate("install_common", "/V0/chassis_motor_2", nullptr));
+        comboBox_2->setItemText(15, QCoreApplication::translate("install_common", "/V0/chassis_motor_3", nullptr));
+        comboBox_2->setItemText(16, QCoreApplication::translate("install_common", "/V0/controlserver_0", nullptr));
+        comboBox_2->setItemText(17, QCoreApplication::translate("install_common", "/V0/grounddetect_0", nullptr));
+        comboBox_2->setItemText(18, QCoreApplication::translate("install_common", "/V0/qosserver_0", nullptr));
+        comboBox_2->setItemText(19, QCoreApplication::translate("install_common", "/V0/safetyserver_0", nullptr));
+        comboBox_2->setItemText(20, QCoreApplication::translate("install_common", "/V0/timesyncserver_0", nullptr));
+        comboBox_2->setItemText(21, QCoreApplication::translate("install_common", "/V0/dataserver_0", nullptr));
 
-        pushButton->setText(QApplication::translate("install_common", "select", nullptr));
-        pushButton_2->setText(QApplication::translate("install_common", "upload", nullptr));
-        label_4->setText(QApplication::translate("install_common", "File:", nullptr));
-        comboBox_3->setItemText(0, QApplication::translate("install_common", "common.yaml", nullptr));
-        comboBox_3->setItemText(1, QApplication::translate("install_common", "service.json", nullptr));
+        pushButton->setText(QCoreApplication::translate("install_common", "select", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("install_common", "upload", nullptr));
+        label_4->setText(QCoreApplication::translate("install_common", "File:", nullptr));
+        comboBox_3->setItemText(0, QCoreApplication::translate("install_common", "common.yaml", nullptr));
+        comboBox_3->setItemText(1, QCoreApplication::translate("install_common", "service.json", nullptr));
 
-        pushButton_3->setText(QApplication::translate("install_common", "load", nullptr));
-        pushButton_4->setText(QApplication::translate("install_common", "save", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("install_common", "load", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("install_common", "save", nullptr));
     } // retranslateUi
 
 };
